ef3311d3c4573cd7511ade573b75a76688cf151d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-12 01:49:40 +0100

    nav to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+
 export default function clientsPage() {
+
+    const clients = [
+        { id: "0", name: "maxano" },
+        { id: "1", name: "semthan" },
+        { id: "2", name: "giff" },
+    ]
+
     return (
         <div>
             <h1>The Clients page</h1>
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
