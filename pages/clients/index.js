5ed9d7e108cff87faac303f7bed5efe09dc890ac
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-12 01:58:48 +0100

    alternative nav to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -15,6 +15,14 @@ export default function clientsPage() {
                 {clients.map((client) => (
                     <li key={client.id}>
                         <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                        {/*                         <Link
+                            href={{
+                                pathname: "clients/[id]",
+                                query: { id: client.id }
+                            }}
+                        >
+                            {client.name}
+                        </Link> */}
                     </li>
                 ))}
             </ul>
