06ff59f67e47c678d8d1afbf5bbb6551a630ba54
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-12 02:07:07 +0100

    nav programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router"
+
 export default function clientProjectsPage() {
+    const router = useRouter()
+
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: '1', clientprojectid: 'projecta' }
+        })
+    }
     return (
         <div>
             <h1>The projects of a given client</h1>
+            <button onClick={loadProjectHandler}>Load project A</button>
         </div>
     )
 }
