3be449fa36e04808e638a8c32a3eb5f845c4a40d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-17 17:56:41 +0100

    render all meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-export default function index() {
-    return (
-        <div>
-            <h1>Home page</h1>
-        </div>
-    )
+import MeetupList from "../components/meetups/MeetupList";
+
+const meetups = [
+    {
+        id: 'm1',
+        title: 'meetup 1',
+        image: 'https://images.unsplash.com/photo-1568992688065-536aad8a12f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80',
+        adress: 'adress',
+        description: 'first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'meetup 2',
+        image: 'https://images.unsplash.com/photo-1568992688065-536aad8a12f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80',
+        adress: 'adress',
+        description: 'second meetup'
+    },
+]
+
+export default function HomePage() {
+    return <MeetupList meetups={meetups} />
 }
