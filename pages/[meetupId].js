265ac8e7df1d22e74f9cbad422154c02712170ae
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-17 18:54:40 +0100

    meetup detail page, component & style

---
+++ pages/[meetupId].js
@@ -0,0 +1,12 @@
+import MeetupDetails from "../components/meetups/MeetupDetails";
+
+export default function MeetupDetailPage() {
+    return (
+        <MeetupDetails
+            image='https://images.unsplash.com/photo-1568992688065-536aad8a12f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80'
+            title=' meetup 1'
+            adress='adress'
+            description='first meetup'
+        />
+    )
+}
