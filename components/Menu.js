f96ffb1ca099f69832eda9d32d10e03dcca01403
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-18 18:37:15 +0100

    working on menu component

---
+++ components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <div
+            className="menu"
+            style={{
+                position: "fixed",
+                top: 0,
+                zIndex: 100
+            }}
+        >
+        </div>
+    )
+}
